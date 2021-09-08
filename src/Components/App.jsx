d3dc481c7f81e3362126cc1387e92afd00e10b82
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-09-08 21:12:12 +0300

    +

---
+++ src/Components/App.jsx
@@ -0,0 +1,63 @@
+/* import trains from "../Data/Trains"; */
+import TrainImage from "./Images"
+
+/* const imgName = trains.map(train => train.desc); */
+
+function App(props) {
+    return (
+        <TrainImage/>
+
+      /*   {imgName.map((b, i) => <Kvadratas key={i} bebras={b}/>)}   */
+    );
+    }
+    
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* class App extends Component {
+    render() {
+
+        const images = trains.map(train => {
+           return <img key={train.img} src={require(`../public/img/${train.img} `)} className="" />
+        });
+
+        return (
+            <div className="container">
+                <footer className="row">
+                    <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <h4>Some text</h4>
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                       { images }
+                    </div>
+                </footer>
+            </div>
+        );
+    }
+} */
+
+
+
+
+
---
+++ src/Components/Images.jsx
@@ -0,0 +1,39 @@
+/* import trains from '../Data/Trains'; */
+
+
+
+/* const imgNr = trains.map(train => train.img); */
+
+function TrainImage(props) {
+    return (
+        <div>
+            <img src={ require(`../Images/${imgNr[i]}`).default } alt = "pic"/>
+        </div>
+    )
+
+
+    
+    /* for(let i=0; i<imgNr.length; i++){
+        console.log(imgNr[i]);
+        <img src="./ "
+        
+    }
+
+
+
+
+        if('Brigadininkas' === props.bebras){            
+            return (
+                <div className = "">            
+                    <b><Bebras bebras={props.bebras}/></b>
+                </div>
+        );
+        }
+    return (
+        <div className = "kvadratas">        
+        <Bebras bebras={props.bebras}/>
+        </div>
+    ); */
+}
+    
+export default TrainImage;
